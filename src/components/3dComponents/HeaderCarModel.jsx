a25954c1987c636fc0091f60177990943153edf5
parsa-aslani <parsa.aslani1390@gmail.com>
2026-08-14 13:38:13 +0330

    fix 3d model texture

--- src/components/3dComponents/HeaderCarModel.jsx
+++ src/components/3dComponents/HeaderCarModel.jsx
@@ -1,27 +1,8 @@
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/Addons.js";
-import * as THREE from "three";
-import { useEffect, useMemo } from "react";
-import useTexture_loader from "./TextureLoader";
+import { useEffect } from "react";
 
 const HeaderCarModel = () => {
-  // texture
-  const fabricTexture = useTexture_loader(1, 1, "Fabric030", true);
-  // materials
-  const fabricMaterial = useMemo(() => {
-    return new THREE.MeshPhysicalMaterial({
-      map: fabricTexture.TextureColor,
-      aoMap: fabricTexture.TextureAO,
-      displacementMap: fabricTexture.TextureDisp,
-      displacementScale: 0.01,
-      roughnessMap: fabricTexture.TextureRoughness,
-      normalMap: fabricTexture.TextureNormal,
-      roughness: 1,
-      metalness: 0,
-      sheen: 1,
-      sheenRoughness: 0.7,
-    });
-  }, [fabricTexture]);
   // model
   const car = useLoader(
     GLTFLoader,
@@ -31,11 +12,11 @@ const HeaderCarModel = () => {
     if (car) {
       car.scene.traverse((child) => {
         if (child.isMesh) {
-          child.material.roughness -= 0.8;
+          child.material.roughness -= 0.5;
         }
       });
     }
-  }, [car, fabricMaterial]);
+  }, [car]);
   return <primitive object={car.scene} scale={90} position={[0, 0.1, -5]} />;
 };
 export default HeaderCarModel;
